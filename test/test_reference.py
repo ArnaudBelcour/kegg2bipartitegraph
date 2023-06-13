0942f3e8e9e1e1be301570ae4c1794d8f33840ed
Arnaud Belcour <arnaud.belcour@gmail.com>
2023-06-13 15:34:42 +0200

    Add test for reaction equation.

---
+++ test/test_reference.py
@@ -0,0 +1,28 @@
+import shutil
+import subprocess
+
+from cobra.io import read_sbml_model
+
+from kegg2bipartitegraph.reference import extract_reaction
+
+def test_extract_reaction_simple():
+    expected_left_compounds = [('C00479', 2)]
+    expected_right_compounds = [('C02948', 1)]
+    reaction_id = 'R00038'
+    equation_text = '2 C00479 <=> C02948'
+    left_compounds, right_compounds = extract_reaction(reaction_id, equation_text)
+
+    assert expected_left_compounds == left_compounds
+    assert expected_right_compounds == right_compounds
+
+
+def test_extract_reaction_n():
+    expected_left_compounds = [('C02321', 1), ('C00007', 1)]
+    expected_right_compounds = [('C21834', 1)]
+    reaction_id = 'R11999'
+    equation_text = 'C02321 + n C00007 <=> n C21834'
+
+    left_compounds, right_compounds = extract_reaction(reaction_id, equation_text)
+
+    assert expected_left_compounds == left_compounds
+    assert expected_right_compounds == right_compounds
